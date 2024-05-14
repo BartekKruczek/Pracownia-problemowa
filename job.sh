8e2afc92007b103ededf9842d9020b37f1a95fc1
Bartłomiej Kruczek <bkruczekk@student.agh.edu.pl>
2024-05-14 20:00:28 +0200

    updating

--- job.sh
+++ job.sh
@@ -3,8 +3,8 @@
 #SBATCH --time=00:15:00
 #SBATCH --account=plgexaile2-gpu-a100
 #SBATCH --partition=plgrid-gpu-a100
-#SBATCH --cpus-per-task=1
-#SBATCH --mem=2GB
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=10GB
 #SBATCH --gres=gpu
 
 source /net/pr2/projects/plgrid/plgglemkin/isap/.venv/bin/activate
