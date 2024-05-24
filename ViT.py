b5612e804c7b7a86243c4390e80c9923f85fd74e
Bartłomiej Kruczek <bkruczekk@student.agh.edu.pl>
2024-05-24 11:01:15 +0200

    Update ViT.py
    Tesseract from Google, works fine

--- ViT.py
+++ ViT.py
@@ -1,7 +1,9 @@
 from PIL import Image
 import pytesseract
 
-print(pytesseract.get_languages())
+print(pytesseract.get_languages(config=''))
 
 # To get text from image
-print(pytesseract.image_to_string(Image.open('pdf_images/page_0.jpg')))
+# print(pytesseract.image_to_string(Image.open('pdf_images/page_0.jpg')))
+
+print(pytesseract.image_to_data(Image.open('pdf_images/page_0.jpg')))
