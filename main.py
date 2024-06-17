2e2dfb262466a01993b114eeb01966b1bc45abb2
Bartłomiej Kruczek <bkruczekk@student.agh.edu.pl>
2024-06-17 12:01:46 +0200

    LCS
    Added feature LCS -> longest common subsequence

--- main.py
+++ main.py
@@ -3,9 +3,15 @@ from utils import Utils
 
 def main():
     data = Data(json_path='lemkin-json-from-html', pdf_path='lemkin-pdf')
+    utils = Utils()
 
     # how many files are there in both directories
     print("Detected {} .json and {} .pdf files".format(data.number_of_files()[0], data.number_of_files()[1]))
+
+    # testing the longest_common_subsequence method
+    a = "abcde"
+    b = "ace"
+    print("The longest common subsequence of '{}' and '{}' is: {}".format(a, b, utils.longest_common_subsequence(a, b)))
 
 if __name__ == '__main__':
     main()
--- utils.py
+++ utils.py
@@ -1,6 +1,17 @@
 class Utils():
-    def __init__(self, load_path = None) -> None:
-        self.load_path = load_path
+    def __init__(self) -> None:
+        pass
 
     def __repr__(self) -> str:
         return "Klasa do obsługi różnych narzędzi"
+    
+    def longest_common_subsequence(self, a, b):
+        """
+        Returns the longest common subsequence of two strings
+        """
+        if len(a) == 0 or len(b) == 0:
+            return 0
+        if a[-1] == b[-1]:
+            return 1 + self.longest_common_subsequence(a[:-1], b[:-1])
+        else:
+            return max(self.longest_common_subsequence(a, b[:-1]), self.longest_common_subsequence(a[:-1], b))
