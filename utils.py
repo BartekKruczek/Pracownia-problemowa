89ed425914719345c8aac376fb496421272c5383
Bartłomiej Kruczek <bkruczekk@student.agh.edu.pl>
2024-06-17 12:18:41 +0200

    minor changes

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
+import os
+
 class Utils():
-    def __init__(self) -> None:
-        pass
+    def __init__(self, json_path: str) -> None:
+        self.json_path = json_path
 
     def __repr__(self) -> str:
         return "Klasa do obsługi różnych narzędzi"
@@ -15,3 +17,16 @@ class Utils():
             return 1 + self.longest_common_subsequence(a[:-1], b[:-1])
         else:
             return max(self.longest_common_subsequence(a, b[:-1]), self.longest_common_subsequence(a[:-1], b))
+        
+    def json_folder_iterator(self):
+        """
+        Iterates over a directory with .json files
+        """
+        files_counter_json = 0
+        for root, dirs, files in os.walk(self.json_path):
+            for dir in dirs:
+                for file in os.listdir(os.path.join(root, dir)):
+                    if file.endswith('.json'):
+                        files_counter_json += 1
+                        print(file)
+        return files_counter_json
