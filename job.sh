eb2c855549e03bd4f24196de35ab8f938b742538
BartekKruczek <bkruczekk@student.agh.edu.pl>
2024-10-19 13:53:48 +0200

    7b-AWQ test is working

--- job.sh
+++ job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --job-name=PP1qwen2-7B
-#SBATCH --time=00:05:00
+#SBATCH --job-name=7B-AWQ
+#SBATCH --time=01:00:00
 #SBATCH --account=plgexaile2-gpu-a100
 #SBATCH --partition=plgrid-gpu-a100
 #SBATCH --cpus-per-task=8
@@ -10,4 +10,4 @@
 module load GCCcore/10.3.0 tesseract/5.4.1 CUDA/12.4.0
 source /net/pr2/projects/plgrid/plgglemkin/isap/Pracownia-problemowa/.venv/bin/activate
 cd /net/pr2/projects/plgrid/plgglemkin/isap/Pracownia-problemowa
-python3 main.py
+python3 test_awq.py
